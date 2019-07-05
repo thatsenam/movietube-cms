4534a8f61c8e5815e60f5b18eeb6d3b464073c85
enam <surelastmail@gmail.com>
2019-07-05 17:34:43 +0600

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './component/movie/movie.component';
+import { LiveComponent } from './component/live/live.component';
+import { MovieRequestComponent } from './component/movie-request/movie-request.component';
+import { FeatureRequestComponent } from './component/feature-request/feature-request.component';
+import { ReportComponent } from './component/report/report.component';
+import { NotfoundComponent } from './component/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    LiveComponent,
+    MovieRequestComponent,
+    FeatureRequestComponent,
+    ReportComponent,
+    NotfoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: MovieComponent },
+      { path: 'live', component: LiveComponent },
+      { path: 'movie-request', component: MovieRequestComponent },
+      { path: 'feature-request', component: FeatureRequestComponent },
+      { path: 'report', component: ReportComponent },
+      { path: 'feature-request', component: FeatureRequestComponent },
+      { path: '**', component: NotfoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/movie-request/movie-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-request',
+  templateUrl: './movie-request.component.html',
+  styleUrls: ['./movie-request.component.css']
+})
+export class MovieRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
